468a063dcb7000ff5392a67e65022ac327a73b41
zepatrik <zepatrik@users.noreply.github.com>
2021-10-13 13:34:49 +0200

    fix: trigger only once

--- src/components/layouts/stats/animated-counter.tsx
+++ src/components/layouts/stats/animated-counter.tsx
@@ -1,37 +1,33 @@
 import numeral from 'numeral'
-import React, { Component } from 'react'
+import React from 'react'
 import CountUp from 'react-countup'
-import { InView } from 'react-intersection-observer'
+import { useInView } from 'react-intersection-observer'
 
 interface PropTypes {
   countTo: number
 }
 
-class AnimatedCounter extends Component<PropTypes> {
-  render() {
-    return (
-      <InView>
-        {({ inView, ref }) => (
-          <div ref={ref}>
-            {inView ? (
-              <CountUp
-                delay={0}
-                start={0}
-                end={this.props.countTo}
-                useEasing
-                duration={3}
-                formattingFn={(v) => numeral(v).format('0.0a')}
-              >
-                {({ countUpRef }) => <span ref={countUpRef} />}
-              </CountUp>
-            ) : (
-              <span>0</span>
-            )}
-          </div>
-        )}
-      </InView>
-    );
-  }
+const AnimatedCounter = ({ countTo }: PropTypes) => {
+  const [ref, inView] = useInView({ delay: 300, triggerOnce: true })
+
+  return (
+    <div ref={ref}>
+      {inView ? (
+        <CountUp
+          delay={0}
+          start={0}
+          end={countTo}
+          useEasing
+          duration={3}
+          formattingFn={(v) => numeral(v).format('0.0a')}
+        >
+          {({ countUpRef }) => <span ref={countUpRef} />}
+        </CountUp>
+      ) : (
+        <span>0</span>
+      )}
+    </div>
+  )
 }
 
 export default AnimatedCounter
